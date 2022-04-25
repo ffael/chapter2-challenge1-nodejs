256e696742ceefa77bf0a52a321ceed3285e897a
Rafael Ferreira <ffael@live.com>
2022-04-24 21:40:53 -0400

    Makes all tests pass!

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,21 @@
-import { Request, Response } from "express";
-
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { Request, Response } from "express"
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase"
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers
+
+    const id = user_id.toString()
+
+    try {
+      const all = this.listAllUsersUseCase.execute({ user_id: id })
+      return response.status(200).json(all)
+    } catch (e) {
+      return response.status(400).json({ error: e.message })
+    }
   }
 }
 
-export { ListAllUsersController };
+export { ListAllUsersController }
